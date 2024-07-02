0d53fd018506a79ac9bbeaddbe833c40815b2af2
Daniela Alves <danieladossantosalves01@gmail.com>
2024-07-02 17:24:46 -0300

    feat: style signup

--- frontend/src/components/SignUp/index.js
+++ frontend/src/components/SignUp/index.js
@@ -1,16 +1,29 @@
 import Input from "../Input";
 import './SignUp.css';
+import React, { useState } from 'react';
+import 'react-phone-input-2/lib/style.css';
+import PhoneInput, {isValidPhoneNumber} from 'react-phone-input-2'
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
 
 function SignUp() {
-
-
+  
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [error, setError] = useState('');
+  
+    const handleChange = (value, country) => {
+      setPhoneNumber(value);
+      if (!isValidPhoneNumber(value, country.iso2)) {
+        setError('Número de telefone inválido');
+      } else {
+        setError('');
+      }
+    };
+  
   return (
     <div className="wrapper">
         <div className="conteiner1">
-            <h1>Não precisa  de nome</h1>
+            <h1>ManaGym</h1>
             <p>Manage your gym network 
             in a simple and intuitive way.</p>
         </div>
@@ -20,6 +33,17 @@ function SignUp() {
                 <Input type="text" name="firstName" id="firstName" label="First name"/>
                 <Input type="text" name="lastName" id="lastName" label="Last name"/> 
                 <Input type="email" name="emailAddress" id="emailAddress" label="Email address" />              
+                <div className="phoneInput">
+                  <PhoneInput
+                    country={'us'}
+                    value={phoneNumber}
+                    onChange={(value, country) => handleChange(value, country)}
+                />
+                 {error && <p style={{ color: 'red' }}>{error}</p>}
+
+                </div>
+                
+                
                 <Input type="password" name="password" id="password" label="Password"/>         
             </form>
          </div>
