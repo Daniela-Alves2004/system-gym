4437ec839530f162ebca7652def52aadff952d32
Daniela Alves <danieladossantosalves01@gmail.com>
2024-07-11 12:27:43 -0300

    feat: update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import SignUp from './components/SignUp';
+import Login from './components/Login';
 import './assets/fonts.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <SignUp />
+      
     </div>
     
+    </>
   );
 }
 
--- frontend/src/components/SignUp/index.js
+++ frontend/src/components/SignUp/index.js
@@ -1,55 +1,45 @@
 import Input from "../Input";
 import './SignUp.css';
-import React, { useState } from 'react';
-import 'react-phone-input-2/lib/style.css';
-import PhoneInput, {isValidPhoneNumber} from 'react-phone-input-2'
-
 
 
 function SignUp() {
-  
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [error, setError] = useState('');
-  
-    const handleChange = (value, country) => {
-      setPhoneNumber(value);
-      if (!isValidPhoneNumber(value, country.iso2)) {
-        setError('Número de telefone inválido');
-      } else {
-        setError('');
-      }
-    };
-  
+
   return (
     <div className="wrapper">
-        <div className="conteiner1">
-            <h1>ManaGym</h1>
-            <p>Manage your gym network 
-            in a simple and intuitive way.</p>
-        </div>
-        <div className="conteiner2">
-            <h2>Sign up now</h2>
-            <form>
-                <Input type="text" name="firstName" id="firstName" label="First name"/>
-                <Input type="text" name="lastName" id="lastName" label="Last name"/> 
-                <Input type="email" name="emailAddress" id="emailAddress" label="Email address" />              
-                <div className="phoneInput">
-                  <PhoneInput
-                    country={'us'}
-                    value={phoneNumber}
-                    onChange={(value, country) => handleChange(value, country)}
-                />
-                 {error && <p style={{ color: 'red' }}>{error}</p>}
-
-                </div>
-                
-                
-                <Input type="password" name="password" id="password" label="Password"/>         
-            </form>
-         </div>
+      <div className="conteiner1">
+        <h1>ManaGym</h1>
+        <p>Manage your gym network
+          in a simple and intuitive way.</p>
+      </div>
+      <div className="conteiner2">
+        <h2>Sign up now</h2>
+        <form>
+          <div className="names"> 
+            <Input type="text" name="firstName" id="firstName" label="Firstname" />
+            <Input type="text" name="lastName" id="lastName" label="Lastname" />
+          </div>
+          <Input type="email" name="emailAddress" id="emailAddress" label="Email address" />
+          <Input type="tel" name="phoneNumber" id="phoneNumber" label="Phone number" />
+          <Input type="password" name="password" id="password" label="Password" />
+          <div className="checkbox-container">
+            <label className="checkbox-label">
+              <input type="checkbox" name="checkbox1" id="checkbox1" />
+              <span>By creating an account, I agree to our <a href="#">Terms of use</a> and <a href="#">Privacy Policy</a></span>
+            </label>
+            <label className="checkbox-label">
+              <input type="checkbox" name="checkbox2" id="checkbox2" />
+              <span>By creating an account, I am also consenting to receive SMS messages and emails, including product new feature updates, events, and marketing promotions.</span>
+            </label>
+          </div>
+         
+            <button type="submit">Sign up</button>
+            <p>Already have an account? <a href=".\login\index.js"> Log in  </a></p>
+          
+        </form>
+      </div>
     </div>
-    
+
   );
- 
+
 }
 export default SignUp;
