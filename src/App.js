29e60d2606d9373b27b2ac9b5fb8f646e580daad
Daniela Alves <danieladossantosalves01@gmail.com>
2024-07-12 14:08:27 -0300

    feat:finishing sugnup page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import './assets/fonts.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <SignUp />
+      <Login />
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
